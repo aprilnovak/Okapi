4795e844b4c84a231a532a1a156223e448cac397
April Novak <anovak@nash.mcs.anl.gov>
2017-06-07 07:44:05 -0500

    initial commit

---
+++ src/base/OkapiMCSApp.C
@@ -0,0 +1,60 @@
+#include "OkapiMCSApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "ModulesApp.h"
+#include "MooseSyntax.h"
+
+template <>
+InputParameters
+validParams<OkapiMCSApp>()
+{
+  InputParameters params = validParams<MooseApp>();
+  return params;
+}
+
+OkapiMCSApp::OkapiMCSApp(InputParameters parameters) : MooseApp(parameters)
+{
+  Moose::registerObjects(_factory);
+  ModulesApp::registerObjects(_factory);
+  OkapiMCSApp::registerObjects(_factory);
+
+  Moose::associateSyntax(_syntax, _action_factory);
+  ModulesApp::associateSyntax(_syntax, _action_factory);
+  OkapiMCSApp::associateSyntax(_syntax, _action_factory);
+}
+
+OkapiMCSApp::~OkapiMCSApp() {}
+
+// External entry point for dynamic application loading
+extern "C" void
+OkapiMCSApp__registerApps()
+{
+  OkapiMCSApp::registerApps();
+}
+void
+OkapiMCSApp::registerApps()
+{
+  registerApp(OkapiMCSApp);
+}
+
+// External entry point for dynamic object registration
+extern "C" void
+OkapiMCSApp__registerObjects(Factory & factory)
+{
+  OkapiMCSApp::registerObjects(factory);
+}
+void
+OkapiMCSApp::registerObjects(Factory & factory)
+{
+}
+
+// External entry point for dynamic syntax association
+extern "C" void
+OkapiMCSApp__associateSyntax(Syntax & syntax, ActionFactory & action_factory)
+{
+  OkapiMCSApp::associateSyntax(syntax, action_factory);
+}
+void
+OkapiMCSApp::associateSyntax(Syntax & /*syntax*/, ActionFactory & /*action_factory*/)
+{
+}
