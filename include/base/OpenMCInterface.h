ecdefba430221d069ee6df7ab2c3cd2c98a87832
April Novak <novak@berkeley.edu>
2017-06-09 14:39:07 -0500

    added better comments to interface file.

--- include/base/OpenMCInterface.h
+++ include/base/OpenMCInterface.h
@@ -11,18 +11,27 @@
 #define FORTRAN_CALL_MOD(name, mod) __##mod##_MOD_##name
 
 namespace OpenMC {
-// need to prevent name mangling between C++ and Fortran, since C++ permits 
-// function overloading, which to distinguish between different versions of
-// a function with the same name, means that the C++ compiler changes the 
-// name in the symbol table in the object file. We need to avoid this so that
-// we can get the names to correctly match up.
+/* need to prevent name mangling between C++ and Fortran, since C++ permits 
+   function overloading, which to distinguish between different versions of
+   a function with the same name, means that the C++ compiler changes the 
+   name in the symbol table in the object file. We need to avoid this so that
+   we can get the names to correctly match up. */
+
   extern "C" {
-    // prototype a simple subroutine not in a module
+  /* Prototype a simple subroutine not in a module. This requires a different
+     macro than a subroutine in a module. This prototype is simply for testing. */
     void FORTRAN_CALL(openmc_fort_func)();
 
-    // prototype a subroutine in a module named "initialize"
+  /* Prototype a subroutine in a module, which requires a different macro than
+     a subroutine not in a module. */
+    
+    /* OpenMC can run in "serial" in two ways - using the MPI wrapper (with a 
+       single process), or _actually_ in serial. "#ifdef MPI" is used within
+       OpenMC to differentiate between these two cases, but because this variable
+       is defined within the OpenMC CMake file, it is not known to MOOSE. I 
+       assume that MPI is defined in OpenMC, such that openmc_init must be called
+       with an intracommunicator. */
     void FORTRAN_CALL_MOD(openmc_init, initialize)(const int &);
-    //void FORTRAN_CALL_MOD(openmc_init, initialize)();
     void FORTRAN_CALL_MOD(run_simulation, simulation)();
     void FORTRAN_CALL_MOD(openmc_finalize, finalize)();
   }
