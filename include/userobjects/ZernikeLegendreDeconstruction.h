d820d75ed67636f0c238be13cf402cca6b1d4b7c
April Novak <novak@berkeley.edu>
2017-06-16 09:37:57 -0500

    changed syntax over.

--- include/userobjects/ZernikeLegendreDeconstruction.h
+++ include/userobjects/ZernikeLegendreDeconstruction.h
@@ -12,9 +12,6 @@ class ZernikeLegendreDeconstruction;
 template<>
 InputParameters validParams<ZernikeLegendreDeconstruction>();
 
-/**
- * This postprocessor computes the projection of solution field to an expansion
- */
 class ZernikeLegendreDeconstruction :
   public ElementIntegralUserObject,
   public MooseVariableInterface
@@ -26,29 +23,16 @@ protected:
   virtual Real computeQpIntegral() override;
   virtual void finalize() override;
     
-  /// Holds the solution at current quadrature points
   const VariableValue & _u;
-  /// Legendre polynomial object. TODO address dynamic casting of this object
-  LegendrePolynomial & _legendre_poly_function;
-  /// Fourier polynomial object.  TODO address dynamic casting of this object
-  ZernikePolynomial & _fourier_poly_function;
-  /// The coordinate directions of the integration for Legendre polynomial
+  LegendrePolynomial & _legendre_function;
+  ZernikePolynomial & _zernike_function;
   int _l_direction;
-  /// The coordinate directions of the itnegration for Fourier polynomials
-  int _f_direction_1;
-  int _f_direction_2;
-  /// The order of the Legendre polynomial
+  int _fdir1;
+  int _fdir2;
   int _l_order;
-  /// The order of the Fourier polynomial
-  int _f_order;
-  /// The name of the aux scalar index by l_order
+  int _n_order;
   std::string _aux_scalar_name;
-  /// The coupled gradient for heat flux
-  const VariableGradient & _coupled_grad;
-  /// Thermal conductivity (diffusion coefficient)
-  const MaterialProperty<Real> & _diffusion_coefficient;
-  /// The name of the post processor that calculates the surface area
-  const PostprocessorValue & _surface_area_pp;
+  const PostprocessorValue & _volume_pp;
 };
 
 #endif
--- src/userobjects/ZernikeLegendreDeconstruction.C
+++ src/userobjects/ZernikeLegendreDeconstruction.C
@@ -5,64 +5,65 @@
 template<>
 InputParameters validParams<ZernikeLegendreDeconstruction>()
 {
+  /* This class inherits from a class that uses the BlockRestrictable parameters, which means
+     that the variable and/or domain blocks to which this integral is to be applied can be
+     specified in the input file using the variable = `` and blocks = `` syntax. */
   InputParameters params = validParams<ElementIntegralUserObject>();
   params.addRequiredCoupledVar("variable", "The name of the variable that will be integrated");
-  params.addRequiredParam<std::string>("legendre_function_name", "Name of function to compute Legendre polynomial value at a point");
-  params.addRequiredParam<std::string>("fourier_function_name", "Name of function to compute Fourier polynomial value at a point");
-  params.addRequiredParam<int>("l_direction", "Direction of integration for Legendre polynomial");
+  params.addRequiredParam<std::string>("legendre_function", \
+    "Name of function to compute Legendre polynomial value at a point.");
+  params.addRequiredParam<std::string>("zernike_function", \
+    "Name of function to compute Zernike polynomial value at a point");
+  params.addRequiredParam<int>("l_direction", \
+    "Direction of integration for Legendre polynomial");
   params.addRequiredParam<int>("l_order", "The order of the Legendre expansion");
-  params.addRequiredParam<int>("f_order", "The order of the Fourier expansion");
-  params.addRequiredParam<std::string>("aux_scalar_name", "Aux scalar to store the Legendre expansion coefficient");
-  params.addParam<MaterialPropertyName>("diffusion_coefficient_name",
-                                        "thermal_conductivity",
-                                        "Property name of the diffusivity (Default: thermal_conductivity)");
-  params.addRequiredParam<std::string>("surface_area_pp", "The name of the post processor that calculates surface area");
+  params.addRequiredParam<int>("n_order", "The order of the Zernike expansion");
+  params.addRequiredParam<std::string>("aux_scalar_name", \
+    "Aux scalar to store the Legendre expansion coefficient");
+  params.addRequiredParam<std::string>("volume_pp", "The name of the post processor that\
+     calculates volume.");
   return params;
 }
 
 ZernikeLegendreDeconstruction::ZernikeLegendreDeconstruction(const InputParameters & parameters) :
-    // TODO we really shouldn't have to dynamic cast into ZernikePolynomial and Legendre Polynomial here
-    // but this was a quick example
     ElementIntegralUserObject(parameters),
     MooseVariableInterface(this, false),
     _u(coupledValue("variable")),
-    _legendre_poly_function(dynamic_cast<LegendrePolynomial&>(_mci_feproblem.getFunction(parameters.get<std::string>("legendre_function_name")))),
-    _fourier_poly_function(dynamic_cast<ZernikePolynomial&>(_mci_feproblem.getFunction(parameters.get<std::string>("fourier_function_name")))),
+    _legendre_function(dynamic_cast<LegendrePolynomial&>\
+      (_mci_feproblem.getFunction(parameters.get<std::string>("legendre_function")))),
+    _zernike_function(dynamic_cast<ZernikePolynomial&>\
+      (_mci_feproblem.getFunction(parameters.get<std::string>("zernike_function")))),
     _l_direction(parameters.get<int>("l_direction")),
     _l_order(parameters.get<int>("l_order")),
-    _f_order(parameters.get<int>("f_order")),
+    _n_order(parameters.get<int>("n_order")),
     _aux_scalar_name(parameters.get<std::string>("aux_scalar_name")),
-    _coupled_grad(coupledGradient("variable")),
-    _diffusion_coefficient(getMaterialProperty<Real>("diffusion_coefficient_name")),
-    _surface_area_pp(getPostprocessorValueByName(parameters.get<std::string>("surface_area_pp")))
-                            
+    _volume_pp(getPostprocessorValueByName(parameters.get<std::string>("volume_pp")))
 {
   addMooseVariableDependency(mooseVariable());
-
-  // For now hard code that l_direction better be 2
-  // TODO come back and put logic in to determine f diections given l directions
-  if ( _l_direction == 2)
+ 
+   if (_l_direction == 0) // Legendre in x-direction, Zernike in y-z
   {
-    _f_direction_1 = 0;
-    _f_direction_2 = 1;
+    _fdir1 = 1;
+    _fdir2 = 2;
   }
-  else
+  else if (_l_direction == 1) // Legendre in y-direction, Zernike in x-z
   {
-    mooseError("Need to implement logic for l direction not equal to 2");
+    _fdir1 = 0;
+    _fdir2 = 2;
   }
-  
-  
+  else // Legendre in z-direction, Zernike in x-y
+  {
+    _fdir1 = 0;
+    _fdir2 = 1;
+  }
 }
 
 Real
 ZernikeLegendreDeconstruction::computeQpIntegral()
 {
-  Real l_poly_val = _legendre_poly_function.getPolynomialValue(_t,_q_point[_qp](_l_direction),_l_order);
-  Real f_poly_val = _fourier_poly_function.getPolynomialValue(_t,_q_point[_qp](_f_direction_1),_q_point[_qp](_f_direction_2), _f_order, _f_order);
-  // There is an added correction factor that accounts for the surface area of the pin
-  // The analytic expression is (2 / R / delta z) but we compute numerically as
-  // (2 / (Surf Area / (2*PI) ) ) = 4PI / Surf Area
-  return -_diffusion_coefficient[_qp] * _coupled_grad[_qp].norm() * l_poly_val * f_poly_val * 4.0*M_PI / _surface_area_pp;
+  Real l_func = _legendre_function.getPolynomialValue(_t,_q_point[_qp](_l_direction),_l_order);
+  Real z_func = _zernike_function.getPolynomialValue(_t,_q_point[_qp](_fdir1),_q_point[_qp](_fdir2), _n_order, _n_order);
+  return _u[_qp] * l_func * z_func * 2.0 * M_PI / _volume_pp;
 }
 void
 ZernikeLegendreDeconstruction::finalize()
