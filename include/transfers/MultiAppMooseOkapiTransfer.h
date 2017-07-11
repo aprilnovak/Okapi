6302024777588f85f18d285a6701f192d9777eb7
April Novak <novak@berkeley.edu>
2017-07-11 09:19:57 -0500

    beginning transfer between Okapi and MOOSE when Okapi is the master app.

---
+++ include/transfers/MultiAppMooseOkapiTransfer.h
@@ -0,0 +1,28 @@
+#ifndef MULTIAPPMOOSEOKAPITRANSFER_H
+#define MULTIAPPMOOSEOKAPITRANSFER_H
+
+// MOOSE includes
+#include "MultiAppTransfer.h"
+
+// Forward declerations
+class MultiAppMooseOkapiTransfer;
+
+template<>
+InputParameters validParams<MultiAppMooseOkapiTransfer>();
+
+class MultiAppMooseOkapiTransfer :
+  public MultiAppTransfer
+{
+public:
+  MultiAppMooseOkapiTransfer(const InputParameters & parameters);
+
+  virtual void execute() override;
+
+protected:
+  std::vector<VariableName> _source_var_names;
+  std::vector<VariableName> _to_aux_names;
+  int _cell;
+  bool _dbg;
+};
+
+#endif /* MULTIAPPMOOSEOKAPITRANSFER_H */
